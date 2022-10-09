3358250b69d787272e590f5322dfa7e9a9e3dd72
TestDevelopeer <testdeveloper7967@gmail.com>
2022-10-09 16:13:39 +0300

    avoid-mutations-and-side-effects-using-functional-

---
+++ JavaScript Algorithms and Data Structures/Functional Programming/avoid-mutations-and-side-effects-using-functional-programming.js
@@ -0,0 +1,9 @@
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+  let newVal = fixedValue + 1;
+  return newVal;
+  // Only change code above this line
+}
